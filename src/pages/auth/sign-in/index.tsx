522e8aa26808d66fce7236a8074cc31dbdb69a27
Denis Sokolov <ostin1982@yandex.ru>
2023-04-15 11:51:53 +0300

    feat/SignIn page layout for all permissions

--- src/pages/auth/sign-in/index.tsx
+++ src/pages/auth/sign-in/index.tsx
@@ -5,7 +5,9 @@ import { Button } from "@/shared/ui/Button";
 import { Input } from "@/shared/ui/Input";
 
 import patternSvg from "./pattern.svg";
+import patternSvg768 from "./pattern-768.svg";
 import logoSvg from "./logo.svg";
+import mailIconSvg from "./mailIcon.svg";
 import styles from "./styles.module.css";
 
 interface Props {
@@ -17,15 +19,19 @@ const SignInPage: FC<Props> = ({ className }) => {
     <>
       <main className={cn(styles.root, className)}>
         <section className={styles.pattern}>
-          <img
-            src={patternSvg}
-            className={styles.images}
-            alt="pattern in blue tones"
-          />
+          <picture>
+            <source srcSet={patternSvg768} media="(min-width: 768px)"></source>
+            <img
+              src={patternSvg}
+              className={styles.images}
+              alt="pattern in blue tones"
+            />
+          </picture>
         </section>
         <section className={styles.container}>
           <div className={styles.logo}>
             <img src={logoSvg} alt="logo" className={styles.logoSvg} />
+            <p className={styles.name}>DSAspro</p>
           </div>
           <div className={styles.module}>
             <div className={styles.info}>
@@ -38,6 +44,15 @@ const SignInPage: FC<Props> = ({ className }) => {
               </Input>
               <Button>Get started</Button>
             </form>
+          </div>
+          <div className={styles.footer}>
+            <p className={styles.text}>© DSAspro 2023</p>
+            <div className={styles.mail}>
+              <img src={mailIconSvg} alt="letter icon" />
+              <a href="mailto: help@dsaspro.com" className={styles.help}>
+                help@dsaspro.com
+              </a>
+            </div>
           </div>
         </section>
       </main>
